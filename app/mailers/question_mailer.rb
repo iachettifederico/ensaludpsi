6a2e17798fd761742d426ede7cd5b078de2f37b7
Federico Iachetti <iachetti.federico@gmail.com>
2011-10-30 01:22:10 -0300

    la puta que lo pario!

--- app/mailers/question_mailer.rb
+++ app/mailers/question_mailer.rb
@@ -1,8 +1,8 @@
 class QuestionMailer < ActionMailer::Base
-  default :to => "info@ensaludpsi.com.ar", :from => "info@ensaludpsi.com.ar"
+  default :to => "info@ensaludpsi.com.ar"
   
   def ask_question(question)
     @question = question
-    mail(:subject => "[ensaludpsi.com.ar] #{question.subject}", :relply_to => question.from)
+    mail(:from => "#{question.name} <#{question.from}>", :subject => "[ensaludpsi.com.ar] #{question.subject}")
   end
 end
--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "gmail.com",
-  :user_name            => "gimenag.psi",
-  :password             => "chinita",
+  :user_name            => "iachetti.federico",
+  :password             => "ecofield1235",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
