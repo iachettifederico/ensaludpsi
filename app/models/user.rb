e8ee5868bed8ffcabbd64e6b397329c57291c2c1
Federico Iachetti <iachetti.federico@gmail.com>
2011-11-06 15:36:57 -0300

    limpié las vistas.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,9 @@ class User < ActiveRecord::Base
 	attr_accessible :name, :email, :password, :password_confirmation
 	attr_accessible :name, :email, :password, :password_confirmation, :admin, :editor, :as => :admin
 	
+	validates :email, :presence => true, :uniqueness => true, :format => { :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i, :message => "debe ser una dirección de email válida" }
+	validates :name, :presence => true
+	
   has_secure_password
-  validates_presence_of :password, :on => :create
+  validates_presence_of :password, :password_confirmation, :on => :create
 end
