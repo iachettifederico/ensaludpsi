2fa35305c2c47135a492beed0f52d26a296c83cf
Federico Iachetti <iachetti.federico@gmail.com>
2011-11-08 01:21:23 -0300

    Index y show de articles funcional.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,8 @@ class ArticlesController < ApplicationController
   # GET /articles.json
   def index
     @articles = Article.all
-
+		@categories = Category.all
+		
     respond_to do |format|
       format.html # index.html.erb
       format.json { render :json => @articles }
@@ -14,7 +15,8 @@ class ArticlesController < ApplicationController
   # GET /articles/1.json
   def show
     @article = Article.find(params[:id])
-
+		@categories = Category.all
+		
     respond_to do |format|
       format.html # show.html.erb
       format.json { render :json => @article }
